972d0c69df3900719c857e30993a9e41cec8c40f
Damian Cozma <damiancozma620@gmail.com>
2025-09-20 23:47:16 +0300

    connect login component to backend

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,11 +1,59 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor(private router: Router) {}
+
+  private http = inject(HttpClient);
+
+  form = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required],
+    }),
+  });
+
+  get usernameIsInvalid() {
+    return (this.form.controls.username.touched &&
+      this.form.controls.username.dirty &&
+      this.form.controls.username.invalid
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+
+  onSubmit() {
+    const credentials = this.form.value;
+
+    this.http.post('http://localhost:8080/api/login',
+      credentials, { responseType: 'text', withCredentials: true }).subscribe({
+      next: () => {
+        console.log('Login successfully');
+        this.router.navigate(['/']);
+      },
+      error: () => {
+        console.log('Login failed');
+      }
+    })
+
+    this.form.reset();
+  }
 }
